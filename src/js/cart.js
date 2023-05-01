03bce1c2a6957a82a32dd7a2ef08a44de4b5c807
sara latorre <saralatorre97@gmail.com>
2023-05-01 17:51:33 +0200

    added total to cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,29 +1,37 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = Array(getLocalStorage("so-cart"));
+  const cartItems = getLocalStorage("so-cart") || [];
 
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  if (cartItems.length > 0) {
+    const total = cartItems.reduce((acc, item) => acc + item.totalPrice, 0);
+    const cartFooter = document.querySelector(".cart-footer");
+    cartFooter.classList.remove("hide");
+    cartFooter.querySelector(".cart-total").textContent = `Total: $${total.toFixed(2)}`;
+  }
 }
 
 function cartItemTemplate(item) {
+  // const quantity = item.quantity ? item.quantity : 1;
+  // const totalPrice = item.totalPrice ? item.totalPrice : item.FinalPrice;
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: ${item.quantity}</p>
+    <p class="cart-card__price">$${item.totalPrice.toFixed(2)}</p>
+  </li>`;
   return newItem;
 }
 
 renderCartContents();
+// localStorage.clear();
--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
+
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || [];
+  const index = cartItems.findIndex((item) => item.Id === product.Id);
+  if (index === -1) {
+    cartItems.push({ ...product, quantity: 1, totalPrice: product.FinalPrice });
+  } else {
+    cartItems[index].quantity += 1;
+    cartItems[index].totalPrice = cartItems[index].quantity * product.FinalPrice;
+  }
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
