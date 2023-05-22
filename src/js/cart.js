a583067e09e53394a009490fcde9d4561662b0e6
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-05-22 17:56:36 -0300

    formated document

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,11 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./shoppingCart.mjs";
 
-
 loadHeaderFooter();
 ShoppingCart();
 
 // let a = document.querySelectorAll(".cart-card__quantity")
-// for (var i = 0; i < a.length; i++) { 
+// for (var i = 0; i < a.length; i++) {
 //     var item = a[i];
 //     console.log(a[i].innerHTML)
 // }
--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,8 +1,11 @@
 import productList from "./productList.mjs";
-import { getParam} from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 
 const productId = getParam("product");
 productList(".product-list", productId);
 
 const productTitle = productId.charAt(0).toUpperCase() + productId.slice(1);
-document.querySelector(".product-category").innerHTML = productTitle.replace("-", " ");
+document.querySelector(".product-category").innerHTML = productTitle.replace(
+  "-",
+  " "
+);
