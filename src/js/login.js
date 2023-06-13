a5334d4b56a20b0c8f842b3fa330fd7b0f2f54c5
sara latorre <saralatorre97@gmail.com>
2023-06-13 08:21:55 +0200

    added login feature

---
+++ src/js/login.js
@@ -0,0 +1,18 @@
+import { loadHeaderFooter, displayTotalItems, getParam } from "./utils.mjs";
+import { login } from "./auth.mjs";
+
+loadHeaderFooter(() => {
+  // callback function will be executed after the template rendering is complete
+  displayTotalItems();
+});
+
+const loginForm = document.getElementById("login-form");
+const redirectParam = getParam("redirect");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.querySelector("input[name='email']").value;
+  const password = document.querySelector("input[name='password']").value;
+  const redirect = redirectParam;
+  login({email, password}, redirect);
+});
---
+++ src/js/orders.js
@@ -0,0 +1,6 @@
+import { loadHeaderFooter, displayTotalItems } from "./utils.mjs";
+
+loadHeaderFooter(() => {
+  // callback function will be executed after the template rendering is complete
+  displayTotalItems();
+});
