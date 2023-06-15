b58f730097a4bea89d9737cfb9a05595d1e29d68
sara latorre <saralatorre97@gmail.com>
2023-06-15 14:07:00 +0200

    modified the orders display

--- src/js/orders.js
+++ src/js/orders.js
@@ -1,8 +1,12 @@
 import currentOrders from "../js/currentOrders.mjs";
-import { loadHeaderFooter } from "../js/utils.mjs";
+import { loadHeaderFooter, displayTotalItems } from "../js/utils.mjs";
 import { checkLogin } from "../js/auth.mjs";
 
-loadHeaderFooter();
+loadHeaderFooter(() => {
+  // callback function will be executed after the template rendering is complete
+  displayTotalItems();
+});
+
 // currentOrders will need to send the token to the server with the request or it will be denied. if checkLogin will return the token upon success
 const token = checkLogin();
 currentOrders("#orders", token);
